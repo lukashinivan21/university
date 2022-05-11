14ce61dff2850bb2551aa0e68985aef4c33896e9
Ivan <uvanes21@yandex.ru>
2022-05-11 06:05:02 +0300

    Work2

--- src/main/java/ru/bestschools/university/model/Student.java
+++ src/main/java/ru/bestschools/university/model/Student.java
@@ -1,18 +1,29 @@
 package ru.bestschools.university.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.util.Objects;
 
+@Entity
 public class Student {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
     private int age;
 
-    public Student(Long id, String name, int age) {
-        this.id = id;
-        this.name = name;
-        this.age = age;
+//    public Student(Long id, String name, int age) {
+//        this.id = id;
+//        this.name = name;
+//        this.age = age;
+//    }
+
+    public Student() {
+
     }
 
     public Long getId() {
